8639920f68f5400317c250b31953fc55faf88ca1
Kaydoash <kdnander@ksu.edu>
2024-12-10 22:59:22 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const button = document.getElementById('changeContent');
+  
+  if (button) {
+    button.addEventListener('click', () => {
+      document.body.style.backgroundColor = '#ADD8E6'; // Change background color
+      alert('Background color changed!');
+    });
+  }
+});
 
